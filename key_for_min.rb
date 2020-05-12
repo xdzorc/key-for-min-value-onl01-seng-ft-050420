c129602baf8e0225d8830b76c14d47c8e78d6e07
DAZUN XING <dazun.xing@gmail.com>
2020-05-12 21:46:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+def key_for_min_value(name_hash)
+  if name_hash=={} 
+    # really interesting, when using =(i know its worng but pass nil)
+    return nil
+  else
+    a=(name_hash.collect {|x,y| y}).min
+    name_hash.collect {|x,y| return x if y==a}
+  end
+  
+end
 end
